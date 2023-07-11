440303245d9e7c360be63edaba447d1ba0121ce9
project2 <104954190+malteshig@users.noreply.github.com>
2023-07-11 11:45:57 +0530

    Update add1.sh

--- add1.sh
+++ add1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "addition of 2 numbers\nEnter the first number"
-read num1
+read $num1
 echo "enter the second number"
-read num2
+read $num2
 sum=`expr $num1 + $num2`
 echo "the sum of two numbers is $sum"
